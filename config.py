814aff536ebfda5d9251f1cf6a1fef8091d2055f
David Sykes <david.sykes70@gmail.com>
2016-05-22 15:04:34 +0000

    rename App_config ref

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ SECRET_KEY = 'freedom_st'
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-config_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),"App_config")
+config_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),"app_config")
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
